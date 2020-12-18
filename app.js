a7b7de4dea3660dcef6e3e2ed1957677303eddf9
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2020-12-18 12:54:03 +0530

    doc:added js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
 
 function clickHandler(){
     console.log("Clicked");
+    console.log("input" , txtInput.value);
 };
 
 //listening to events
