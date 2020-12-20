a60b8f61b2fafa2b976bc343c0f47472b2815a9b
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2020-12-21 00:12:54 +0530

    doc:updated

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+outputDiv.innerText = " Somesh Banerjee";
 
 console.log(txtInput);
 
