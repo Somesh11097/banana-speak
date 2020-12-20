64e0613fc6a12ea166c6d2e88786d7c658caae1d
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2020-12-21 00:19:32 +0530

    input / output processing done

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-outputDiv.innerText = " Somesh Banerjee";
 
-console.log(txtInput);
 
-function clickHandler(){
-    console.log("Clicked");
-    console.log("input" , txtInput.value);
+
+function clickHandler() {
+    outputDiv.innerText = "ajsjsjs: " + txtInput.value;
+    
 };
 
 //listening to events
