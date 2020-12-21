31bf57e879fd5ed2c9da6b7d9d189bb229b15e7a
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2020-12-21 15:52:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,39 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+
+}
+
+// How to handle Error
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something is wrong with server: try again after some time");
+}
+
+
 
 //handling input and output
 function clickHandler() {
-    outputDiv.innerText = "ajsjsjs: " + txtInput.value;
-    
+   
+    var inputText = txtInput.value;   //taking Input
+
+
+
+    //calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response =>response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        
+       })
+    .catch(errorHandler)
 };
 
 //listening to events
